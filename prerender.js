33f87a222523ec8febd44281c440ba77b9dac467
Kaciras <kaciras@outlook.com>
2018-04-26 16:57:32 +0800

    use driver.wait for waiting for render complete.

--- prerender.js
+++ prerender.js
@@ -3,28 +3,28 @@ const urlib = require("url");
 
 class Renderer {
 
-	constructor(chromePath) {
-		const chromeCapabilities = selenium.Capabilities.chrome();
-		chromeCapabilities.set('chromeOptions', {
-			/*
-			 * "--no-sandbox" is required for Linux without GUI
-			 * "--disable-gpu" is required for Windows,
-			 * remove "--headless" will shows browser GUI. It's useful for debug.
-			 */
-			args: ["--headless", "--disable-extensions", "--enable-logging --v=1", "--no-sandbox", "--disable-gpu"]
-		});
+	constructor(chromePath, headless) {
+		const args = [
+			"--disable-extensions",
+			"--window-size=1366,768",
+			"--enable-logging --v=1",
+			"--no-sandbox", // required for Linux without GUI
+			"--disable-gpu" // required for Windows
+		];
 
-		chromeCapabilities.set("chrome.binary", chromePath);
+		if (headless) {
+			args.push("--headless")
+		}
 
-		/* accept all certs */
-		chromeCapabilities.set("acceptInsecureCerts", true);
+		const chromeCapabilities = selenium.Capabilities.chrome()
+			.set('chromeOptions', {args})
+			.set("chrome.binary", chromePath)
+			.set("acceptInsecureCerts", true); //accept all certs
 
 		this.driver = new selenium.Builder()
 			.forBrowser('chrome')
 			.withCapabilities(chromeCapabilities)
 			.build();
-
-		process.on("exit", () => this.driver.quit());
 	}
 
 	/**
@@ -34,84 +34,40 @@ class Renderer {
 	 * @return {Promise<string>} html text of page.
 	 */
 	async render(url) {
-
 		/* put some query paramters to tell the script in page that they are being pre-rendering */
 		const nUrl = new urlib.URL(url);
 		nUrl.searchParams.set("isPrerender", "true");
 		nUrl.searchParams.delete("prerender");
 
 		await this.driver.get(nUrl.toString());
+		const body = await this.driver.findElement(selenium.By.css("body"));
 
-		/* waiting fixed time for render complete */
-		// await driver.sleep(3000);
-
-		/* or polling the specified flag */
-		let body = await this.driver.findElement(selenium.By.css("body"));
-		await new FluntWait(this.driver)
-			.withTimeout(3000)
-			.pollingEvery(200)
-			.throwOnTimeout(false)
-			.until(async d => await body.getAttribute("data-render-complete") === "true");
+		/* Waiting for data-render-complete attribute append to body */
+		await this.driver.wait(new selenium.Condition("for render complete", async () => {
+			return await body.getAttribute("data-render-complete") === "true";
+		}), 5000);
 
 		return await (await this.driver
-			.findElement(selenium.By.css("html")))
-			.getAttribute("outerHTML");
+			.findElement(selenium.By.css("html"))).getAttribute("outerHTML");
 	}
 
 	/**
-	 * get console logs from Developer Tools/Console.
+	 * Fetches available log entries for Developer Tools(F12)/Console.
 	 *
-	 * @return {promise.Thenable<Array<logging.Entry>>} logs entry
+	 * @return {promise.Thenable<Array<logging.Entry>>} log entries
 	 */
 	logs() {
 		return this.driver.manage().logs().get("browser");
 	}
-}
 
-
-class FluntWait {
-
-	constructor(driver) {
-		this.driver = driver;
-		this.tot = true;
-	}
-
-	throwOnTimeout(tot) {
-		this.tot = tot;
-		return this;
-	}
-
-	withTimeout(timeout) {
-		this.timeout = timeout;
-		return this;
-	}
-
-	pollingEvery(span) {
-		this.span = span;
-		return this;
-	}
-
-	async until(checker) {
-		const deadline = new Date().getTime() + this.timeout;
-		const span = this.span;
-		const driver = this.driver;
-		const trowOnTimeout = this.tot;
-
-		async function doWait() {
-			if (new Date().getTime() > deadline) {
-				if (!trowOnTimeout) {
-					return;
-				}
-				throw new Error("wait timeout");
-			}
-			if (await checker(driver)) {
-				return;
-			}
-			await driver.sleep(span);
-			await doWait();
-		}
-
-		await doWait();
+	/**
+	 * Terminates the browser session.
+	 *
+	 * @return {promise.Thenable<void>} the promise
+	 * @see IWebDriver.quit
+	 */
+	close() {
+		return this.driver.quit()
 	}
 }
 
